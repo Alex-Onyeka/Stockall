de1edce723a840ee3af443223791b6a83c3054f6
Alex Onyeka <alexonyekasm@gmail.com>
2025-05-27 22:39:17 +0100

    Some Changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3292954141"
+    serviceWorkerVersion: "501881977"
   }
 });
