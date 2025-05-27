e247c649b2dd13b2b89ede9049660cf5c2c8aa94
Alex Onyeka <alexonyekasm@gmail.com>
2025-05-27 11:36:21 +0100

    Some changed

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3565093050"
+    serviceWorkerVersion: "1557896668"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -325,7 +325,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f00be8bf1261b558f048a6c00fbb287e
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a37caffb6a5396ba5f3ead64ec2f7954",
+"flutter_bootstrap.js": "27a5538d7cb5f73199b31aa4dc5a79fd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
