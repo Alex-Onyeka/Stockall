005a7b33b17bc54250aeb123100c876a9add6fdf
Alex Onyeka <alexonyekasm@gmail.com>
2025-06-05 21:39:56 +0100

    Changes

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -114,20 +114,18 @@ class _HomeState extends State<Home> {
                     child: CircularProgressIndicator(),
                   ),
                 );
-              }
-              // else if (userSnapshot.hasError) {
-              //   return Scaffold(
-              //     body: EmptyWidgetDisplayOnly(
-              //       title: 'An Error Occurred',
-              //       subText:
-              //           'We couldn\'t load your employee data.',
-              //       icon: Icons.clear,
-              //       theme: theme,
-              //       height: 30,
-              //     ),
-              //   );
-              // }
-              else if (userSnapshot.data == null &&
+              } else if (userSnapshot.hasError) {
+                return Scaffold(
+                  body: EmptyWidgetDisplayOnly(
+                    title: 'An Error Occurred',
+                    subText:
+                        'We couldn\'t load your employee data.',
+                    icon: Icons.clear,
+                    theme: theme,
+                    height: 30,
+                  ),
+                );
+              } else if (!userSnapshot.hasData &&
                   !_navigated) {
                 WidgetsBinding.instance
                     .addPostFrameCallback((_) {
