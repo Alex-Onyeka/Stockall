e1368647a4dbf8f40a68ab09d6f6b7fb78f773b1
Alex Onyeka <alexonyekasm@gmail.com>
2025-06-07 14:39:04 +0100

    Changes

--- lib/pages/authentication/forgot_password_page/enter_new_password/platforms/enter_new_password_mobile.dart
+++ lib/pages/authentication/forgot_password_page/enter_new_password/platforms/enter_new_password_mobile.dart
@@ -149,9 +149,9 @@ class _EnterNewPasswordMobileState
                                   'Are you sure you want to proceed?',
                               title: 'Proceed?',
                               action: () async {
-                                Navigator.of(
-                                  safeContex,
-                                ).pop();
+                                // Navigator.of(
+                                //   safeContex,
+                                // ).pop();
                                 setState(() {
                                   isLoading = true;
                                 });
@@ -170,17 +170,15 @@ class _EnterNewPasswordMobileState
                                   showSuccess = true;
                                 });
 
-                                // cleanUpUrl(
-                                //   '/',
-                                // ); // works safely on all platforms
+                                cleanUpUrl('/#/');
 
-                                if (safeContex.mounted) {
-                                  Navigator.pushNamedAndRemoveUntil(
-                                    context,
-                                    '/#/',
-                                    (route) => false,
-                                  );
-                                }
+                                // if (safeContex.mounted) {
+                                //   Navigator.pushNamedAndRemoveUntil(
+                                //     context,
+                                //     '/#/',
+                                //     (route) => false,
+                                //   );
+                                // }
 
                                 setState(() {
                                   isLoading = false;
