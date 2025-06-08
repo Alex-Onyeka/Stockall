a4b254c8920bc448ae42999a814267bf1e64a137
Alex Onyeka <alexonyekasm@gmail.com>
2025-06-08 08:45:22 +0100

    Added public logo url

--- lib/helpers/clean_up_url_web.dart
+++ lib/helpers/clean_up_url_web.dart
@@ -1,31 +1,16 @@
 // ignore: avoid_web_libraries_in_flutter
-// import 'package:web/web.dart' as web;
-
-// /// Removes the query parameters from the current URL (used on Flutter Web).
-// void cleanUpUrl(String link) {
-//   final cleanPath = web.window.location.pathname;
-
-//   // This updates the browser history without reloading the page
-//   // web.window.open(link, '_blank');
-//   web.window.history.replaceState(null, '', cleanPath);
-// }
-
 import 'package:web/web.dart' as web;
 
-/// Removes query parameters and fragments from the current URL (used on Flutter Web),
-/// while keeping the path and domain intact.
+/// Removes the query parameters from the current URL (used on Flutter Web).
 void cleanUpUrl() {
-  final origin =
-      web
-          .window
-          .location
-          .origin; // e.g. https://yourdomain.com
-  final pathname =
-      web.window.location.pathname; // e.g. /reset-password
+  final cleanPath = web.window.location.pathname;
 
-  // Rebuild clean URL
-  final cleanUrl = '$origin$pathname';
-
-  // Replace current URL in browser without reloading
-  web.window.history.replaceState(null, '', cleanUrl);
+  // This updates the browser history without reloading the page
+  // web.window.open(link, '_blank');
+  web.window.history.replaceState(null, '', cleanPath);
 }
+
+// // Used only on web
+// void cleanUpUrl() {
+//   web.window.history.replaceState(null, '', '/');
+// }
