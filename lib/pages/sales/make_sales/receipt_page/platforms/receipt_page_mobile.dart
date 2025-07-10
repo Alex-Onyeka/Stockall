2079da90dcfcf7eaacec1b968bde78c8976f149b
Alex Onyeka <alexonyekasm@gmail.com>
2025-07-10 10:02:21 +0100

    Changes

--- lib/pages/sales/make_sales/receipt_page/platforms/receipt_page_mobile.dart
+++ lib/pages/sales/make_sales/receipt_page/platforms/receipt_page_mobile.dart
@@ -1115,7 +1115,7 @@ class _ReceiptDetailsContainerState
                               print('Downloading Pdf');
                               downloadPdfWeb(
                                 pdfBytes,
-                                'Stockall ${widget.mainReceipt.isInvoice ? 'Invoice' : 'Receipt'} : ${widget.mainReceipt.id}',
+                                'Stockall ${widget.mainReceipt.isInvoice ? 'Invoice' : 'Receipt'} : ${widget.mainReceipt.id}.pdf',
                               );
                             }
                             if (context.mounted) {
