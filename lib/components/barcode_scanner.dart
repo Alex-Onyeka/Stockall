26b89063dd75fab220ecde9fcb28abe45a0615b6
Alex Onyeka <alexonyekasm@gmail.com>
2025-07-14 02:26:33 +0100

    Worked on Barcode scanner for iOS

--- lib/components/barcode_scanner.dart
+++ lib/components/barcode_scanner.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:stockall/constants/app_bar.dart';
